8743b6e76f05aa6b0901b6d7846b9c8bc0f89c14
priyanshu345 <99245316+priyanshu345@users.noreply.github.com>
2023-11-18 13:32:44 -0500

    merges

--- load_board/load_board/views.py
+++ load_board/load_board/views.py
@@ -1,9 +1,12 @@
 import json
 from django.http import JsonResponse
-from django.shortcuts import render
 from . import mqtt_client
 
 def read_message(request):
     request_data = json.loads(request.body)
-    rc, mid = mqtt_client.client.publish(request_data['topic'], request_data['msg'])
-    return JsonResponse({'code': rc})
+    return JsonResponse({'code': request_data})
+
+# def publish_message(request):
+    # request_data = json.loads(request.body)
+    # to fix the duplicate messages
+    # rc, mid = mqtt_client.client.publish(request_data['topic'], request_data['msg'])
